d92a24e9319fab5a531604c7b062de5a5ee7ec5d
Charles Owuala <51397595+CHARLE123455@users.noreply.github.com>
2025-02-06 12:16:34 +0100

    `Removed authMiddleware and replaced with authenticateJWT and authencticateRole in userRoutes.js`

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,13 +2,12 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-const authMiddleware = require('../middlewares/authMiddleware');
 const { authenticateJWT, authencticateRole} = require('../middleware/authMiddleware');
 
 router.post('/', authenticateJWT, authencticateRole('admin'), userController.createUser);
-router.get('/', authMiddleware, userController.getAllUsers);
-router.get('/:id', authMiddleware, userController.getUserById);
-router.put('/:id', authMiddleware, userController.updateUser);
-router.delete('/:id', authMiddleware, roleMiddleware('admin'), userController.deleteUser);
+router.get('/', authenticateJWT, authencticateRole('admin'), userController.getAllUsers);
+router.get('/:id',authenticateJWT, authencticateRole('admin'), userController.getUserById);
+router.put('/:id', authenticateJWT, authencticateRole('admin'), userController.updateUser);
+router.delete('/:id', authenticateJWT, authencticateRole('admin'), userController.deleteUser);
 
 module.exports = router;
